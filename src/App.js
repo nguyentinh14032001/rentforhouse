0ef4e766093eab5cd8dad612820ba4402ba76a55
Nguyen Tinh <nguyentinh14032001@gmail.com>
2022-09-15 07:55:36 +0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
 function App() {
-  return <div></div>;
+  return (
+    // <Routes>
+    //   {/* <Route path="/sign-up" ></Route> */}
+    // </Routes>
+    <div></div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,30 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        body: ["Epilogue", "sans-serif;"],
+        primary: ["Epilogue", "sans-serif;"],
       },
+      colors:{
+        primary: "#1DC071",
+        secondary:"#6F49FD",
+        "text-1": "#171725",
+        "text-2": "#4B5264",
+        "text-3": "#808191",
+        "text-4": "#B2B3BD",
+        "icon-color": "#A2A2A8",
+        white:"#FFFFFF",
+        whiteSoft:"#FCFBFF",
+        graySoft:"#FCFCFC",
+        strock:"#F1F1F3",
+        lite:"#FCFCFD",
+        error:"#EB5757",
+        darkbg:"#13131A",
+        darkSecondary:"#1C1C24",
+        softDark:"#22222C",
+        darkSoft:"#24242C",
+        darkStroke:"#3A3A43",
+        
+
+      }
     },
   },
   plugins: [],
