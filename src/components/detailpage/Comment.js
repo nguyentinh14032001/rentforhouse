a11851d8edf6ae09592f68448809ca3788254cda
unknown <gionganhqn@gmail.com>
2022-10-03 07:54:17 +0700

    update detail latest

--- src/components/detailpage/Comment.js
+++ src/components/detailpage/Comment.js
@@ -4,9 +4,9 @@ import cmtAva1 from "../../assets/images/demoAva.jpg";
 const Comment = () => {
   return (
     <>
-      <div className="m-4 flex w-full">
+      <div className=" m-8 flex w-full">
         <img src={cmtAva1} alt="" className="mr-4 h-[60px] w-[55px]" />
-        <div className="cmt-item flex w-[90%] flex-col bg-white p-4">
+        <div className="cmt-item relative flex w-[90%] flex-col bg-white p-4">
           <h1 className="font-bold text-[#1359C1]">Nguyên Tính</h1>
           <div className="mx-auto mb-2 h-[1px] w-[100%] bg-black opacity-20"></div>
           <p className="text-sm opacity-90">
@@ -17,6 +17,12 @@ const Comment = () => {
             assumenda et, iusto laborum nesciunt minus rerum quidem magnam,
             quisquam omnis voluptas. Cupiditate?
           </p>
+          <div className="absolute bottom-[-1.5rem] left-2 flex items-baseline text-[14px]">
+            <button className="mr-2">Reply</button>
+            <button className="mr-2">Like</button>
+            <button className="mr-2">Dislike</button>
+            <button className="mr-2 text-[24px] font-bold">...</button>
+          </div>
         </div>
       </div>
     </>
