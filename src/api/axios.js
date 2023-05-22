cc8a7610a0c6108c32478a642f8a04c82a0fb143
dinhtho2001 <phamdinhtho.0105@gmail.com>
2023-05-22 10:31:43 +0700

    update base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,9 @@ const { default: axios } = require("axios");
 
 export default axios.create({
   baseURL:
-    "http://rentforhouse-env.eba-sypmxta3.ap-southeast-1.elasticbeanstalk.com",
+    // "http://rentforhouse-env.eba-sypmxta3.ap-southeast-1.elasticbeanstalk.com",
+    "http://localhost:5000",
 });
 export const baseURL =
-  "http://rentforhouse-env.eba-sypmxta3.ap-southeast-1.elasticbeanstalk.com";
+  // "http://rentforhouse-env.eba-sypmxta3.ap-southeast-1.elasticbeanstalk.com";
+  "http://localhost:5000";
